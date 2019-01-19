efefcdab6316a87f848057d961dd86dc75d42857
yuse <lyzwhh@qq.com>
2019-01-19 16:53:12 +0800

    修改二维码文件名

--- app/Services/PassService.php
+++ app/Services/PassService.php
@@ -95,9 +95,10 @@ class PassService
             }
 
             $info = $pass->car_number;
-            $fileName = $pass->name.' '.$info;
+            $fileName = $pass->department.'-'.$pass->name.'-'.$info;
             $fileName = iconv('UTF-8', 'GBK', $fileName);
             QrCode::format('png')->encoding('UTF-8')->size(1000)->merge('/public/logo.png',.3)->errorCorrection('H')->generate('1#'.$info, '../public/QRCodes/'.$fileName.'.png');
+                                                                                                                    //generate（二维码内容，地址）
             $dst_path = 'QRCodes/'.$fileName.'.png';
             $img = imagecreatefromstring(file_get_contents($dst_path));
             $font = 'font/simhei.ttf';
